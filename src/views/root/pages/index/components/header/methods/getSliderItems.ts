67c32905fcd3857119dd846e693ef20ed589be6d
F_edoS <m-f-a1999@yandex.ru>
2026-05-28 18:55:29 +0700

    Fix header slider

--- src/views/root/pages/index/components/header/methods/getSliderItems.ts
+++ src/views/root/pages/index/components/header/methods/getSliderItems.ts
@@ -3,6 +3,13 @@ import I from '../types.ts';
 const getSliderItems: I['getSliderItems'] = function () {
     const { mainContent } = this.props;
 
+    if (mainContent.components.anounce.carousel.thumbsExtra.length === 0) {
+        return mainContent.components.anounce.carousel.thumbs.map((t) => ({
+            thumb: t,
+            title: '',
+        }));
+    }
+
     return mainContent.components.anounce.carousel.thumbsExtra.map((t) => ({
         thumb: t[0],
         title: t[1],
